98aadb83f782f5bb8ed29b3a627c13ac41da54cd
leongatys <leon.gatys@bethgelab.org>
2016-06-24 14:10:41 -0700

    only compute activations on layers needed

--- ComputeActivations.lua
+++ ComputeActivations.lua
@@ -36,7 +36,7 @@ local function main(params)
     
     -- Load network from caffemodel
     local loadcaffe_backend = params.backend
-    local cnn = loadcaffe.load('none', params.caffe_model, params.backend):float()
+    local cnn = loadcaffe.load('network', params.caffe_model, params.backend):float()
     cnn = set_datatype(cnn, params.gpu)
 
     -- Load images
@@ -44,24 +44,45 @@ local function main(params)
     local images = f:all()['images']
     f:close()
     images = set_datatype(images, params.gpu)
+    
+    -- Set up new network only using necessary layers 
+    local net = nn.Sequential()
+    local layers = nil 
+    if params.layers == 'all' then
+        net = cnn:clone()
+    else
+        layers = params.layers:split(",")
+        local next_layer_ndx = 1
+        for i = 1, #cnn do
+            if next_layer_ndx <= length(layers) then
+                local layer = cnn:get(i)
+                local layer_name = layer.name
+                net:add(layer)
+                if layer_name == layers[next_layer_ndx] then
+                    next_layer_ndx = next_layer_ndx + 1
+                end
+            end
+        end
+    end
+    cnn = nil
+    collectgarbage()
 
     -- Pass images through the network
-    cnn:forward(images)
+    net:forward(images)
 
     local f = hdf5.open(params.output_file, 'w')
     if params.layers == 'all' then
-        for i = 1, #cnn do
-            local layer = cnn:get(i)
+        for i = 1, #net do
+            local layer = net:get(i)
             local layer_name = layer.name
             local layer_type = torch.type(layer)
             f:write(layer_name, layer.output:double())
         end
     else
-        local layers = params.layers :split(",")
         next_layer_ndx = 1
-        for i = 1, #cnn do
+        for i = 1, #net do
             if next_layer_ndx <= length(layers) then
-                local layer = cnn:get(i)
+                local layer = net:get(i)
                 local layer_name = layer.name
                 local layer_type = torch.type(layer)
                 if layer_name == layers[next_layer_ndx] then
@@ -71,13 +92,7 @@ local function main(params)
             end
         end
     end
-    print(f:all()['relu1_1']:size())
     f:close()
-    g = hdf5.open(params.output_file, 'r')
-    print(g:all()['relu1_1']:size())
-    g:close()
--- local debugger = require('fb.debugger')
--- debugger.enter()
 end
 
 local params = cmd:parse(arg)
