d8581090126b990e66f37b1d413ab1da963198c5
leongatys <leon.gatys@bethgelab.org>
2016-12-14 10:51:26 +0100

    included model download

---
+++ Models/download_models.sh
@@ -0,0 +1,4 @@
+cd Models
+wget -c --no-check-certificate https://bethgelab.org/media/uploads/deeptextures/vgg_normalised.caffemodel
+wget -c http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_19_layers.caffemodel
+cd ..
