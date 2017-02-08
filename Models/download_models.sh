1b94459e8d6594cf208fe29062bcd9b0466b5bf3
leongatys <leon.gatys@bethgelab.org>
2017-02-08 11:31:10 +0100

    download conv part only of VGG network

--- Models/download_models.sh
+++ Models/download_models.sh
@@ -1,4 +1,5 @@
 cd Models
 wget -c --no-check-certificate https://bethgelab.org/media/uploads/deeptextures/vgg_normalised.caffemodel
-wget -c http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_19_layers.caffemodel
+wget -c --no-check-certificate https://bethgelab.org/media/uploads/stylecontrol/VGG_ILSVRC_19_layers_conv.caffemodel
+# wget -c http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_19_layers.caffemodel
 cd ..
